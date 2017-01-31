466b16db83df73869b5e28eee86615124cc360f0
Szympan93 <szymon.m93@gmail.com>
2017-01-31 09:23:37 +0100

    dodanie testów różnego kosztu przejścia

--- nai/Assets/Script/Pathfinding/Editor/MapTest.cs
+++ nai/Assets/Script/Pathfinding/Editor/MapTest.cs
@@ -66,5 +66,47 @@ namespace Pathfinding.Test
             List<Node> nodes = map.FindPath(0, 0, 4, 4);
             Assert.IsTrue(nodes.Count == 0);
         }
+
+        [Test]
+        public void PassableVariableCostTest()
+        {
+            Map map = new Map(5, 5);
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    map[x, y] = new Tile(1, true);
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                map[i, 1] = new Tile(2, true);
+            }
+
+            List<Node> nodes = map.FindPath(0, 0, 4, 4);
+            Assert.IsTrue(nodes.Count == 5);
+        }
+
+        [Test]
+        public void UnpassableVariableCostTest()
+        {
+            Map map = new Map(5, 5);
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    map[x, y] = new Tile(1, true);
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                map[i, 1] = new Tile(20, true);
+            }
+
+            List<Node> nodes = map.FindPath(0, 0, 4, 4);
+            Assert.IsTrue(nodes.Count == 8);
+        }
     }
 }
