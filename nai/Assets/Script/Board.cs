a06e376a6c4e1ed6acd6f3578403ca8cce289c4d
Szympan93 <szymon.m93@gmail.com>
2017-01-31 14:33:09 +0100

    odczytywanie mapy bitmapy

---
+++ nai/Assets/Script/Board.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Texture2D MapTex;
+    public Vector2 CostRange;
+    public Field FieldPrefab;
+
+    public Pathfinding.Map Map;
+
+    void Awake()
+    {
+        Map = new Map(MapTex.width, MapTex.height);
+        for (int x = 0; x < Map.W; x++)
+        {
+            for (int y = 0; y < Map.H; y++)
+            {
+                Color color = MapTex.GetPixel(x, y);
+                Tile tile;
+                if (color.b != 1f)
+                {
+                    tile = new Tile(color.g * (CostRange.y-CostRange.x) + CostRange.x, true);
+                }
+                else
+                {
+                    tile = new Tile(0, false);
+                }
+                Map[x, y] = tile;
+                Field field = Instantiate(FieldPrefab.gameObject, new Vector3(x - Map.W/2f, 0, y-Map.H/2f), Quaternion.identity, transform).GetComponent<Field>();
+                field.X = x;
+                field.Y = y;
+                field.Tile = tile;
+            }
+        }
+    }
+}
---
+++ nai/Assets/Script/Field.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Field : MonoBehaviour, IPointerClickHandler
+{
+    [NonSerialized]
+    public int X;
+    [NonSerialized]
+    public int Y;
+
+    public Gradient Gradient;
+    public Color WallColor;
+
+    private SpriteRenderer _sr;
+    private Board _board;
+    private Tile _tile;
+
+    public SpriteRenderer SR
+    {
+        get
+        {
+            if (_sr == null) _sr = GetComponentInChildren<SpriteRenderer>();
+            return _sr;
+        }
+    }
+
+    public Board Board
+    {
+        get
+        {
+            if (_board == null) _board = GetComponentInParent<Board>();
+            return _board;
+        }
+    }
+
+    public Tile Tile
+    {
+        get { return _tile; }
+        set
+        {
+            _tile = value;
+            _update();
+        }
+    }
+
+    private void _update()
+    {
+        if (Tile.Walkable)
+        {
+            SR.color = Gradient.Evaluate((_tile.Cost - Board.CostRange.x)/(Board.CostRange.y - Board.CostRange.x));
+        }
+        else
+        {
+            SR.color = WallColor;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+}
